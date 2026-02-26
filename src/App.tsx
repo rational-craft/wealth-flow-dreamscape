2b7e2a2bbe253deaa04fb56e0278b1d984601266
rational-craft <taylorlemmon@gmail.com>
2026-02-25 19:17:38 -0800

    Fix React entrypoint and router shell wiring

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppShell from "@/layouts/AppShell";
+import DashboardPage from "@/pages/DashboardPage";
 import ExpensesPage from "@/pages/ExpensesPage";
+import IncomePage from "@/pages/IncomePage";
 import NotFoundPage from "@/pages/NotFoundPage";
+import SettingsPage from "@/pages/SettingsPage";
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AppShell>
-        <Routes>
-          <Route path="/" element={<ExpensesPage />} />
+      <Routes>
+        <Route element={<AppShell />}>
+          <Route path="/" element={<DashboardPage />} />
+          <Route path="/income" element={<IncomePage />} />
+          <Route path="/expenses" element={<ExpensesPage />} />
+          <Route path="/settings" element={<SettingsPage />} />
           <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </AppShell>
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
